6aaf3b5b7b00827d591c0e2c600b582e3da198de
DevAlya <abagarmeryem@gmail.com>
2026-03-02 15:19:40 +0000

    commit

--- app/user_manager.py
+++ app/user_manager.py
@@ -1,26 +1,17 @@
 class UserManager:
-
     def __init__(self):
-        self.users = []
-
+        self.users=[]
     def add_user(self, username):
         if not username:
-            raise ValueError("Le nom d'utilisateur est obligatoire")
-
+            raise ValueError("Le nom d'utilisateur est obligatoire!!")
         if username in self.users:
             raise ValueError("Utilisateur déjà existant")
-
-        self.users.append(username)
-
+        self.users.remove(username)
     def remove_user(self, username):
         if username not in self.users:
-            raise ValueError("Utilisateur introuvable")
-
+            raise ValueError("Utilisateur introvable!")
         self.users.remove(username)
-
     def count_users(self):
         return len(self.users)
-
-
 def count_total_users(users):
     return len(users)
